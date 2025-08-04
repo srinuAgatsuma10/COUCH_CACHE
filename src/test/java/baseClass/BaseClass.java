7e71e2c42030025c9a18af9ee79fd6031fd13101
ggsrinivascm035@gmail.com <GiridharaSrinivas>
2025-08-04 12:21:17 +0530

    Creating BaseClass

---
+++ src/test/java/baseClass/BaseClass.java
@@ -0,0 +1,27 @@
+package baseClass;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseClass {
+
+	public static WebDriver driver;
+	
+	@BeforeClass
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.shopist.io/");
+		driver.manage().window().maximize();
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+}
